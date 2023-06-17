77759546581d72ddc453fc8644e3155a50de3f86
Cristian Romanescu <cristian@romanescu.ro>
2023-06-17 17:56:24 +0300

    Set v 2.7.0 in code

--- src/main/java/edw/olingo/service/ServiceInformation.java
+++ src/main/java/edw/olingo/service/ServiceInformation.java
@@ -19,7 +19,7 @@ public class ServiceInformation {
 
 	public static final String UPDATE_URL = "http://www.informea.org/api.properties";
 	public static final int VERSION_MAJOR = 2;
-	public static final int VERSION_MINOR = 5;
+	public static final int VERSION_MINOR = 7;
 	public static final int VERSION_REVISION = 0;
 	public static final boolean VERSION_BETA = true;
 
